975838e9d6f053238171e5f16e8d19210ef3939c
ik-joong <chang.ikjoong@gmail.com>
2024-01-21 21:43:58 +0100

    0.1

---
+++ test/CarTest.java
@@ -0,0 +1,136 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class CarTest {
+    static Saab95 saab;
+    static Volvo240 volvo;
+
+    @Before
+    public void init() {
+        saab = new Saab95();
+        volvo = new Volvo240();
+    }
+
+
+    @Test
+    public void testGas () {
+
+        saab.gas(0.1);
+        assertTrue(saab.getCurrentSpeed()>0);
+    }
+
+
+    @Test
+    public void testBrake () {
+
+        saab.currentSpeed=1;
+
+        saab.brake(1);
+        assertTrue(saab.getCurrentSpeed() ==0);
+
+
+    }
+
+
+    @Test
+    public void testMaxSpeed () {
+        saab.currentSpeed=saab.getEnginePower();
+        saab.incrementSpeed(10);
+
+        assertTrue(saab.getCurrentSpeed()==saab.getEnginePower());
+
+    }
+
+
+    @Test
+    public void testMinSpeed () {
+        saab.currentSpeed=0;
+        saab.decrementSpeed(10);
+
+        assertEquals(0, saab.getCurrentSpeed(), 0.0);
+    }
+
+
+    @Test
+    public void testTurn () {
+        saab.orientation= new Point(0,1);
+
+        saab.turnLeft();
+        assertEquals(saab.orientation, new Point(1,0));
+        saab.turnLeft();
+        assertEquals(saab.orientation, new Point(0,-1));
+        saab.turnLeft();
+        assertEquals(saab.orientation, new Point(-1,0));
+
+        saab.turnRight();
+        assertEquals(saab.orientation, new Point(0,-1));
+        saab.turnRight();
+        assertEquals(saab.orientation, new Point(1,0));
+        saab.turnRight();
+        assertEquals(saab.orientation, new Point(0,1));
+
+
+    }
+
+
+    @Test
+    public void testMove () {
+        saab.coordination=new Point2D.Double(0,0);
+        saab.incrementSpeed(saab.getEnginePower());
+
+        saab.move();
+
+
+        assertTrue(saab.coordination.y > 0 && saab.coordination.x ==0);
+
+        saab.turnRight();
+        saab.move();
+
+
+        assertTrue(saab.coordination.y > 0 && saab.coordination.x < 0);
+
+    }
+    @Test
+    public void testTurbo () {
+        saab.coordination=new Point2D.Double(0,0);
+        saab.orientation=new Point(1,0);
+
+        saab.gas(1);
+
+        saab.setTurboOff();
+        saab.move();
+        double noTurboX = saab.coordination.x;
+
+        saab.stopEngine();
+        saab.coordination=new Point2D.Double(0,0);
+        saab.setTurboOn();
+        saab.gas(1);
+
+        saab.move();
+        assertTrue(saab.coordination.x > noTurboX);
+
+
+
+
+    }
+
+    @Test
+    public void testColor() {
+        Color currentColor = volvo.getColor();
+
+        volvo.setColor(Color.red);
+
+        assertTrue(currentColor!=volvo.getColor());
+        assertTrue(volvo.getColor()==Color.red);
+
+    }
+
+
+}
