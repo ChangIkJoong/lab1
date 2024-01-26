8b08ee681f7311f2344e4bff61399849a9938caa
ik-joong <chang.ikjoong@gmail.com>
2024-01-26 19:45:39 +0100

    0.8 updated to not re-scale DPI for Mac.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("sun.java2d.uiScale", "1.0");
+        System.setProperty("sun.java2d.dpiaware", "false");
+
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
